5ffb11f63082f5c23cd56cb080e53261c49e1e51
eser.sahin <eser.sahin@pyrotech.com.tr>
2023-12-20 14:57:50 +0300

    Check message identifier is idempotent

--- src/TbdDevelop.Kafka.Outbox/InMemoryMessageOutbox.cs
+++ src/TbdDevelop.Kafka.Outbox/InMemoryMessageOutbox.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
 using TbdDevelop.Kafka.Abstractions;
 using TbdDevelop.Kafka.Outbox.Contracts;
 
@@ -6,6 +7,11 @@ namespace TbdDevelop.Kafka.Outbox;
 
 public class InMemoryMessageOutbox : IMessageOutbox
 {
+    private readonly ILogger<InMemoryMessageOutbox> _logger;
+    public InMemoryMessageOutbox(ILogger<InMemoryMessageOutbox> logger)
+    {
+        _logger = logger;
+    }
     private readonly ConcurrentDictionary<Guid, IOutboxMessage> _outbox = new();
 
     public async Task PostAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
@@ -15,8 +21,26 @@ public class InMemoryMessageOutbox : IMessageOutbox
         {
             var message = new OutboxMessage<TEvent>(@event);
 
+            if (IsIdempotent(message.Identifier))
+            {
+                var storedMessage = GetMessageForKey(message.Identifier);
+                //Console.WriteLine($"Operation with key '{message.Identifier}' and message '{storedMessage}' is already processed. Skipping.");
+                _logger.LogInformation($"Operation with key '{message.Identifier}' and message '{storedMessage}' is already processed. Skipping.");
+            }
+
             _outbox.TryAdd(message.Identifier, message);
         }, cancellationToken);
+    }
+
+    private bool IsIdempotent(Guid key)
+    {
+        return _outbox.ContainsKey(key);
+    }
+
+    private IOutboxMessage GetMessageForKey(Guid key)
+    {
+        _outbox.TryGetValue(key, out var storedMessage);
+        return storedMessage; // possible null reference return
     }
 
     public Task<IOutboxMessage?> RetrieveNextMessage(CancellationToken cancellationToken = default)
@@ -24,8 +48,8 @@ public class InMemoryMessageOutbox : IMessageOutbox
         return Task.Run(() =>
         {
             var nextMessage = (from message in _outbox.Values
-                orderby message.AddedOn
-                select message).FirstOrDefault();
+                               orderby message.AddedOn
+                               select message).FirstOrDefault();
 
             return nextMessage;
         }, cancellationToken);
