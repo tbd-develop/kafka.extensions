8e53d6b946ada65fd7d1cd59b4fef15d6962fbf5
Terry Burns-Dyson <terrybd@gmail.com>
2024-04-25 21:27:21 -0400

    Add integration test program for outbox sample

---
+++ samples/outbox-sql-sample/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using outbox_sql_sample;
+using TbdDevelop.Kafka.Abstractions;
+using TbdDevelop.Kafka.Extensions.Infrastructure;
+using TbdDevelop.Kafka.Outbox.Infrastructure;
+using TbdDevelop.Kafka.Outbox.SqlServer.Context;
+using TbdDevelop.Kafka.Outbox.SqlServer.Extensions;
+using Testcontainers.MsSql;
+
+var msSqlContainer = new MsSqlBuilder()
+    .WithImage("mcr.microsoft.com/mssql/server:2019-CU18-ubuntu-20.04")
+    .Build();
+
+await msSqlContainer.StartAsync();
+
+var builder = new HostBuilder()
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddKafka()
+            .AddOutboxPublisher(configure =>
+            {
+                configure
+                    .UseSqlServerOutbox(msSqlContainer.GetConnectionString())
+                    .WithDefaultPublisher();
+            });
+    });
+
+var app = builder.Build();
+
+app.ConfigureSqlOutbox();
+
+var publisher = app.Services.GetRequiredService<IEventPublisher>();
+
+await publisher.PublishAsync(new SampleEvent { Name = "Hello, World" });
+
+var factory = app.Services.GetRequiredService<IDbContextFactory<OutboxDbContext>>();
+
+await using var context = factory.CreateDbContext();
+
+var outboxMessages = await context.OutboxMessages.ToListAsync();
+
+Console.WriteLine($"Message Count: {outboxMessages.Count}");
+
+await app.RunAsync();
---
+++ samples/outbox-sql-sample/SampleEvent.cs
@@ -0,0 +1,10 @@
+﻿using TbdDevelop.Kafka.Abstractions;
+
+namespace outbox_sql_sample;
+
+public class SampleEvent : IEvent
+{
+    public string Name { get; set; } = null!;
+    public Guid EventId { get; }
+    public DateTime OccurredOn { get; }
+}
