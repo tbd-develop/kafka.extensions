f8ebdd23dd25a3702f6900db3bce7cdb74e00a5b
Terry Burns-Dyson <terrybd@gmail.com>
2024-07-07 15:42:45 -0400

    Provide ability for consumers to configure the topic name, overriding the event type lookup

--- src/TbdDevelop.Kafka.Extensions/Infrastructure/Builders/DispatchingConsumerConfigurationBuilder.cs
+++ src/TbdDevelop.Kafka.Extensions/Infrastructure/Builders/DispatchingConsumerConfigurationBuilder.cs
@@ -4,6 +4,7 @@ using TbdDevelop.Kafka.Abstractions;
 using TbdDevelop.Kafka.Extensions.Configuration;
 using TbdDevelop.Kafka.Extensions.Consumption;
 using TbdDevelop.Kafka.Extensions.Contracts;
+using TbdDevelop.Kafka.Extensions.Infrastructure.Exceptions;
 
 namespace TbdDevelop.Kafka.Extensions.Infrastructure.Builders;
 
@@ -14,19 +15,21 @@ public class DispatchingConsumerConfigurationBuilder(
 {
     private readonly List<ITopicConsumer> _consumers = [];
 
-    public DispatchingConsumerConfigurationBuilder AddEventReceiver<TEvent, TConsumer>()
+    public DispatchingConsumerConfigurationBuilder AddEventReceiver<TEvent, TConsumer>(string? topic = null)
         where TEvent : class, IEvent
         where TConsumer : IEventReceiver<TEvent>
     {
-        if (configuration.TryGetTopicFromEventType<TEvent>(out string? topic))
+        if (topic is null && !configuration.TryGetTopicFromEventType<TEvent>(out topic))
         {
-            _consumers.Add(new TopicConsumer<TEvent>(
-                topic!,
-                configuration.Consumer,
-                serviceProvider.GetRequiredService<TConsumer>(),
-                loggerFactory.CreateLogger<TopicConsumer<TEvent>>()
-            ));
+            throw new TopicConfigurationException($"No topic found for event type {typeof(TEvent).Name}");
         }
+
+        _consumers.Add(new TopicConsumer<TEvent>(
+            topic!,
+            configuration.Consumer,
+            serviceProvider.GetRequiredService<TConsumer>(),
+            loggerFactory.CreateLogger<TopicConsumer<TEvent>>()
+        ));
 
         return this;
     }
