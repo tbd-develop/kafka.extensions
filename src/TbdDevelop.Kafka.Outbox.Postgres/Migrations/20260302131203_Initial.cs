b43ff520941c272bbd97d8523b2ac5f1ffcf2ed6
Terry Burns-Dyson <terrybd@gmail.com>
2026-03-02 08:12:54 -0500

    Include DBContextFactory for running migrations.
    Run initial migrations.

---
+++ src/TbdDevelop.Kafka.Outbox.Postgres/Migrations/20260302131203_Initial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TbdDevelop.Kafka.Outbox.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "outbox");
+
+            migrationBuilder.CreateTable(
+                name: "KafkaMessagingOutbox",
+                schema: "outbox",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Key = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    Topic = table.Column<string>(type: "text", nullable: true),
+                    DateAdded = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "TIMEZONE('utc', NOW())"),
+                    DateProcessed = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KafkaMessagingOutbox", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KafkaMessagingOutbox",
+                schema: "outbox");
+        }
+    }
+}
